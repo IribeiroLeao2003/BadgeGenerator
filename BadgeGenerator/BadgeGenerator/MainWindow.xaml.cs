4e40524d7ea5ab2eedd9105ddfcc5cc4ebf1232f
IribeiroLeao2003 <96347353+IribeiroLeao2003@users.noreply.github.com>
2024-04-30 17:37:27 -0400

    Image is succefully being collected

--- BadgeGenerator/BadgeGenerator/MainWindow.xaml.cs
+++ BadgeGenerator/BadgeGenerator/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.IO; 
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +15,51 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace BadgeGenerator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+
+        private double dpiX = 96;
+        private double widthMax = 2.13;
+        private double heightMax = 3.38;
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void getPhotoFileLocation_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if ((bool)openFileDialog.ShowDialog())
+            {
+                string filename = openFileDialog.FileName;
+                using (Bitmap image = new Bitmap(filename))
+                {
+
+   
+                    int requiredWidth = (int)(widthMax * dpiX);
+                    int requiredHeight = (int)(heightMax * dpiX);
+
+                    
+                    if (image.Width != requiredWidth || image.Height != requiredHeight)
+                    {
+                        MessageBox.Show("The image size must be exactly 2.13 x 3.38 inches.", "Invalid Image Size", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    else
+                    {
+                        fileName.Text = Path.GetFileName(filename);
+                    }
+                }
+            }
         }
     }
 }
